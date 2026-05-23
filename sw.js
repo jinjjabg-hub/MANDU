b3c113291934a5bf7fb4ca081ca33eb52a3e99b2
jinjjabg-hub <jinjjabg@gmail.com>
2026-05-24 04:43:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'mandu-v1';
+const ASSETS = ['/MANDU/', '/MANDU/index.html', '/MANDU/manifest.json', '/MANDU/icon-192.png', '/MANDU/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
